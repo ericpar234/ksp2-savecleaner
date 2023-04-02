77ecefc01b37a61b55a4ef27abf16fc9abc1988b
ericpar234 <36040410+ericpar234@users.noreply.github.com>
2023-04-01 20:22:15 -0400

    Update ksp2-savecleaner.py
    Check encdoded dictionary

--- ksp2-savecleaner.py
+++ ksp2-savecleaner.py
@@ -4,7 +4,6 @@ import os
 file_name = 'original.json'
 new_file_name = 'edited.json'
 
-starting_size = os.path.getsize(file_name)
 
 def remove_extra_object_events_old(j):
     ut_list = []
@@ -32,15 +31,16 @@ def remove_extra_object_events(j):
     objects_removed = 0
     total_object_events = len(j["TravelLogData"]["ObjectEvents"])
     for idx, obj in enumerate(j["TravelLogData"]["ObjectEvents"]):
-        if obj['UT'] in ut_list:
+        enc = tuple(obj.items())
+        if enc in ut_list:
             j["TravelLogData"]["ObjectEvents"].pop(idx)
             objects_removed = objects_removed + 1
         else:
-            ut_list.append(obj['UT'])
-            print("Unique ObjectEvent found {}".format(obj['UT']))
+            ut_list.append(enc)
+            print("Unique ObjectEvent found {}".format(enc))
         
         if idx % 10000 == 0:
-            print("Object Events removed: {}/{} {:.2f}%".format(objects_removed, total_object_events, (objects_removed/total_object_events) * 100))
+            print("{:.2f}% complete. Object Events removed: {}/{}".format((idx/total_object_events) * 100, objects_removed, total_object_events))
 
     print("Final Object Events removed: {}/{}".format(objects_removed, total_object_events))
     return j
@@ -57,7 +57,8 @@ if __name__ == "__main__":
     with open(new_file_name, 'w', encoding='utf-8') as f:
         f.write(json.dumps(j, indent=2))
 
-    new_size = os.path.getsize(file_name)
+    new_size = os.path.getsize(new_file_name)
+    starting_size = os.path.getsize(file_name)
 
     print("Old save size: {}".starting_size)
     print("New save size: {}".new_size)
